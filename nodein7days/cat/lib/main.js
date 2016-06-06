95a0b164836f5215ed1682578737c8a4312bd8e4
yuanjunwen <yjw6174@gmail.com>
2016-06-06 23:21:07 +0800

    [0606] 提交npm afan 包,增加 node 项目

---
+++ nodein7days/cat/lib/main.js
@@ -0,0 +1,14 @@
+/**
+ * Created: yuanjunwen
+ * on 6/6/16.
+ */
+/**
+ * 主函数,一个程序的入口
+ */
+
+var count = require('util/count.js');
+var count2 = require('util/count.js');
+
+console.log(count.count());
+console.log(count2.count());
+console.log(count.count());
---
+++ nodein7days/util/count.js
@@ -0,0 +1,7 @@
+var i = 0;
+function count(){
+    "use strict";
+    return i++;
+}
+
+exports.count =  count;
